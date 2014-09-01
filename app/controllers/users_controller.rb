4c0de2f29e6020c8cf168b6dc63018e3ec7a9860
MIR <mightykiwi@yahoo.com>
2014-09-01 11:03:13 -0700

    updated strong params to include password password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
