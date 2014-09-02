b23e1493b68d7046520c2c4c601e4ea03ac4809a
MIR <mightykiwi@yahoo.com>
2014-09-01 17:47:36 -0700

    add relation user has many data_sets

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -1,4 +1,5 @@
 class DataSet < ActiveRecord::Base
+  belongs_to :user
   validates :name, presence: true
   validates :json_data, presence: true
   validates :node, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :data_sets
   validates :email, presence: true
   validates :email, uniqueness: true
 end
