a8eef4ad370ead2ccb509055e1c6324bebc1aff3
MIR <mightykiwi@yahoo.com>
2014-09-01 17:58:46 -0700

    add data_set controller new/create

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -3,4 +3,23 @@ class DataSetsController < ApplicationController
     @data_sets = DataSet.all
   end
 
+  def new
+    @data_set = DataSet.new
+  end
+
+  def create
+    @data_set = current_user.data_sets.new(user_params)
+    if @data_set.save
+      redirect_to root_path, notice: "Thanks for submitting!"
+    else
+      render "new"
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:data_set).permit(:name, :json_data)
+  end
+
 end
