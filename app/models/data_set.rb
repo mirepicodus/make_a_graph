67721a8b21e6b82e8d356823f3d67a44caba1c0e
MIR <mightykiwi@yahoo.com>
2014-09-01 17:16:36 -0700

    add name validation for data_set

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -1,4 +1,5 @@
 class DataSet < ActiveRecord::Base
+  validates :name, presence: true
   validates :json_data, presence: true
   validates :node, presence: true
 end
--- spec/data_set_spec.rb
+++ spec/data_set_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe DataSet do
+  it { should validate_presence_of :name }
   it { should validate_presence_of :json_data }
   it { should validate_presence_of :node }
 end
