41bc214ffd32ea7c4e701e7e765f30af0efff78b
MIR <mightykiwi@yahoo.com>
2014-09-06 11:53:02 -0700

    add alchemy graph method

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -3,4 +3,19 @@ class DataSet < ActiveRecord::Base
   validates :name, presence: true
   validates :json_data, presence: true
   validates :node, presence: true
+
+  def data_to_alchemy
+    nodes = self.json_data.each_with_index do |element, index|
+        element["caption"] = element[self.node]
+        element["id"] = index
+    end
+    node_index = []
+    nodes.each {|node| node_index << node["id"]}
+    permutated_nodes = node_index.permutation(2).to_a
+    edges = permutated_nodes.map {|node1, node2| {source: node1, target: node2}}
+    alchemy_data = {nodes: nodes, edges: edges}
+    alchemy_data.to_json
+  end
+
 end
+
